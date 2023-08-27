ac765465f1c69e6d57f57fe9acbe7a7cba45340f
literandltx <romanskovronn@gmail.com>
2023-08-27 19:31:55 +0300

    add: book, repository, service

--- src/main/java/com/example/bookstore/BookStoreApplication.java
+++ src/main/java/com/example/bookstore/BookStoreApplication.java
@@ -1,13 +1,31 @@
 package com.example.bookstore;
 
+import com.example.bookstore.model.Book;
+import com.example.bookstore.service.BookService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class BookStoreApplication {
+    @Autowired
+    private BookService bookService;
 
     public static void main(String[] args) {
         SpringApplication.run(BookStoreApplication.class, args);
     }
 
+    @Bean
+    public CommandLineRunner commandLineRunner() {
+        return args -> {
+            Book book = new Book();
+            book.setTitle("book");
+            bookService.save(book);
+
+            System.out.println(bookService.findAll());
+            System.out.println("LETS GOOOOOOOOOOOOO..........");
+        };
+    }
 }
