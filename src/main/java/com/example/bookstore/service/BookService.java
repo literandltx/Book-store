25aeb6d59d70b6f3717a8275b58b0e0f04d9e6ce
literandltx <romanskovronn@gmail.com>
2023-09-04 15:54:34 +0300

    refactor bookService

--- src/main/java/com/example/bookstore/service/BookService.java
+++ src/main/java/com/example/bookstore/service/BookService.java
@@ -1,12 +1,13 @@
 package com.example.bookstore.service;
 
-import com.example.bookstore.model.Book;
+import com.example.bookstore.dto.BookDto;
+import com.example.bookstore.dto.CreateBookRequestDto;
 import java.util.List;
 import org.springframework.stereotype.Service;
 
 @Service
 public interface BookService {
-    Book save(Book book);
+    BookDto save(CreateBookRequestDto requestDto);
 
-    List<Book> findAll();
+    List<BookDto> findAll();
 }
--- src/main/java/com/example/bookstore/service/BookServiceImpl.java
+++ src/main/java/com/example/bookstore/service/BookServiceImpl.java
@@ -1,25 +1,28 @@
 package com.example.bookstore.service;
 
-import com.example.bookstore.model.Book;
+import com.example.bookstore.dto.BookDto;
+import com.example.bookstore.dto.CreateBookRequestDto;
+import com.example.bookstore.mapper.BookMapper;
 import com.example.bookstore.repository.BookRepository;
 import java.util.List;
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
+@RequiredArgsConstructor
 @Service
 public class BookServiceImpl implements BookService {
     private final BookRepository bookRepository;
+    private final BookMapper bookMapper;
 
-    public BookServiceImpl(BookRepository bookRepository) {
-        this.bookRepository = bookRepository;
+    @Override
+    public BookDto save(CreateBookRequestDto requestDto) {
+        return bookMapper.toDto(bookRepository.save(bookMapper.toModel(requestDto)));
     }
 
     @Override
-    public Book save(Book book) {
-        return bookRepository.save(book);
-    }
-
-    @Override
-    public List<Book> findAll() {
-        return bookRepository.findAll();
+    public List<BookDto> findAll() {
+        return bookRepository.findAll().stream()
+                .map(bookMapper::toDto)
+                .toList();
     }
 }
