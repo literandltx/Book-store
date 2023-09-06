3dfc472ade1f3ba6ce668ee92d06c07fd8abccb4
literandltx <romanskovronn@gmail.com>
2023-09-06 16:26:16 +0300

    add soft delete + endpoint

--- src/main/java/com/example/bookstore/controller/BookController.java
+++ src/main/java/com/example/bookstore/controller/BookController.java
@@ -5,11 +5,14 @@ import com.example.bookstore.dto.CreateBookRequestDto;
 import com.example.bookstore.service.BookService;
 import java.util.List;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
 @RequiredArgsConstructor
@@ -26,6 +29,12 @@ public class BookController {
     @GetMapping("/{id}")
     BookDto getBookById(@PathVariable Long id) {
         return bookService.findById(id);
+    }
+
+    @ResponseStatus(value = HttpStatus.NO_CONTENT)
+    @DeleteMapping("/{id}")
+    public void delete(@PathVariable Long id) {
+        bookService.deleteById(id);
     }
 
     @PostMapping
