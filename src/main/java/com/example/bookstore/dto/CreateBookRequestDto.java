53f182ba70291ab50dc3f469d9e15280ea7acaeb
literandltx <romanskovronn@gmail.com>
2023-09-07 21:48:11 +0300

    add isbn validation + refactor constraint

--- src/main/java/com/example/bookstore/dto/CreateBookRequestDto.java
+++ src/main/java/com/example/bookstore/dto/CreateBookRequestDto.java
@@ -1,17 +1,21 @@
 package com.example.bookstore.dto;
 
+import com.example.bookstore.validation.Isbn;
+import jakarta.validation.Constraint;
 import jakarta.validation.constraints.Min;
+import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
 import java.math.BigDecimal;
 import lombok.Data;
 
 @Data
 public class CreateBookRequestDto {
-    @NotNull
+    @NotBlank
     private String title;
-    @NotNull
+    @NotBlank
     private String author;
-    @NotNull
+    @NotBlank
+    @Isbn
     private String isbn;
     @NotNull
     @Min(0)
