4d0e9226b77f7ce17b4c88c1ab847426187a2512
literandltx <romanskovronn@gmail.com>
2023-09-14 20:19:21 +0300

    add access by role (for admin@...)

--- src/main/java/com/example/bookstore/model/User.java
+++ src/main/java/com/example/bookstore/model/User.java
@@ -48,7 +48,11 @@ public class User implements UserDetails {
 
     @Override
     public Collection<? extends GrantedAuthority> getAuthorities() {
-        return List.of(new SimpleGrantedAuthority("ADMIN"));
+        if (email.startsWith("admin@")) {;
+            return List.of(new SimpleGrantedAuthority("ROLE_ADMIN"));
+        }
+
+        return List.of(new SimpleGrantedAuthority("ROLE_USER"));
     }
 
     public String getUsername() {
