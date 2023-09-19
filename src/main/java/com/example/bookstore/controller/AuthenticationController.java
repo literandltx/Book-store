12cd291014c1542d5e84b5a2d8e979b39122ddbb
literandltx <romanskovronn@gmail.com>
2023-09-19 23:42:44 +0300

    checkstyle cleanup

--- src/main/java/com/example/bookstore/controller/AuthenticationController.java
+++ src/main/java/com/example/bookstore/controller/AuthenticationController.java
@@ -16,7 +16,8 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-@Tag(name = "User authentication manager", description = "Endpoints for managing user authentication")
+@Tag(name = "User authentication manager",
+        description = "Endpoints for managing user authentication")
 @RestController
 @RequestMapping("/auth")
 @RequiredArgsConstructor
