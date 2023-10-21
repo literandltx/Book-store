8f7ff61b30f267ec158e7b901b53a865e11ef293
literandltx <romanskovronn@gmail.com>
2023-10-21 23:29:23 +0300

    add category repository tests

---
+++ src/test/java/com/example/bookstore/repository/CategoryRepositoryTest.java
@@ -0,0 +1,33 @@
+package com.example.bookstore.repository;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.test.context.jdbc.Sql;
+
+@DataJpaTest
+@Sql(scripts = {
+        "classpath:databases/category/01-insert-2-categories.sql"},
+        executionPhase = Sql.ExecutionPhase.BEFORE_TEST_METHOD)
+@Sql(scripts = {
+        "classpath:databases/category/02-delete-categories.sql"},
+        executionPhase = Sql.ExecutionPhase.AFTER_TEST_METHOD)
+@AutoConfigureTestDatabase(replace = AutoConfigureTestDatabase.Replace.NONE)
+public class CategoryRepositoryTest {
+    @Autowired
+    private CategoryRepository categoryRepository;
+
+    @Test
+    public void existsById_ReturnFalse() {
+        Assertions.assertFalse(categoryRepository.existsById(-1L));
+        Assertions.assertFalse(categoryRepository.existsById(100L));
+    }
+
+    @Test
+    public void existsById_ReturnTrue() {
+        Assertions.assertTrue(categoryRepository.existsById(1L));
+        Assertions.assertTrue(categoryRepository.existsById(2L));
+    }
+}
